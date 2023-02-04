4ff4de50dce5ce8d206951f89636e9d0ed8565ae
Fernando Jimenez <fgjv06@gmail.com>
2023-02-04 16:35:52 -0300

    JS - Getting values from HTML

---
+++ script.js
@@ -0,0 +1,6 @@
+const displayLastValue = document.getElementById('last-value');
+const displayCurrentValue = document.getElementById('current-value');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+const calculator = new Calculator();
